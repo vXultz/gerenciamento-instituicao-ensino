38db12c906604226be37b0114aff28fb7a3f7e27
Vitor Schultz <vitorserta@gmail.com>
2024-03-01 13:03:08 -0300

    first commit

---
+++ src/main/java/br/com/fullstack/education/gerenciamentoinstituicaoensino/GerenciamentoInstituicaoEnsinoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.gerenciamentoinstituicaoensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoInstituicaoEnsinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoInstituicaoEnsinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/education/gerenciamentoinstituicaoensino/GerenciamentoInstituicaoEnsinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.education.gerenciamentoinstituicaoensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoInstituicaoEnsinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
