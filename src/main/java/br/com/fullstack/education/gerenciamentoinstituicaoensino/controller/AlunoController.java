0481a8bbc4be0b4dcca9ae4a1b13b4fe2db1f828
Vitor Schultz <vitorserta@gmail.com>
2024-03-01 21:47:22 -0300

    Ex 6

---
+++ src/main/java/br/com/fullstack/education/gerenciamentoinstituicaoensino/controller/AlunoController.java
@@ -0,0 +1,28 @@
+package br.com.fullstack.education.gerenciamentoinstituicaoensino.controller;
+
+import br.com.fullstack.education.gerenciamentoinstituicaoensino.model.AlunoModel;
+import br.com.fullstack.education.gerenciamentoinstituicaoensino.service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("alunos")
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<AlunoModel> get() {
+        return alunoService.buscarTodos();
+    }
+
+    @PostMapping
+    public AlunoModel post(@RequestBody AlunoModel aluno) throws Exception {
+        return alunoService.salvar(aluno);
+    }
+}
